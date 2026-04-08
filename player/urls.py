2fc4ff92157304800091f5a270ae9c9bdf1159cc
Svyatoslav344 <sviatoslav.yurkiv-pi2203@kep.nung.edu.ua>
2026-04-08 23:08:43 +0300

    Update project

---
+++ player/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('auth/', include('social_django.urls', namespace='social')),
+    path('logout/', views.logout_view, name='logout'),
+]
